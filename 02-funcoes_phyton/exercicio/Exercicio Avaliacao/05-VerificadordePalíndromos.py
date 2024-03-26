daac24488d9a574d3e1b36ac259434207f6a7525
Anna82639 <126599816+Anna82639@users.noreply.github.com>
2024-03-26 14:00:08 -0300

    Update 05-VerificadordePalíndromos.py

--- 02-funcoes_phyton/exercicio/Exercicio Avaliacao/05-VerificadordePalíndromos.py
+++ 02-funcoes_phyton/exercicio/Exercicio Avaliacao/05-VerificadordePalíndromos.py
@@ -1,1 +1,13 @@
 # Verificador de Palíndromos
+
+def verifica_palindromo(texto):
+    texto = texto.lower().replace(" ", "")  
+    return texto == texto [::-1]  
+
+def main():
+    palavra = input("Digite uma palavra ou frase para verificar se é um palíndromo: ")
+    if verifica_palindromo(palavra):
+        print(f"{palavra} é um palíndromo!")
+    else:
+        print(f"{palavra} não é um palíndromo.")
+main()
